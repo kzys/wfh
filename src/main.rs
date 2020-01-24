4fa2a3e99fe54570febf457d719e6374a3c72a06
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2020-01-24 07:17:51 -0800

    Build a basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,88 @@
+extern crate ignore;
+extern crate notify;
+
+#[macro_use]
+extern crate structopt;
+
+use ignore::gitignore;
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use std::fs;
 use std::io;
+use std::sync::mpsc::channel;
 use std::thread;
 use std::time;
 
 mod pbar;
 
-fn main() -> io::Result<()> {
-    let entries = fs::read_dir(".")?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "wfh", about = "synchronize files as you edit")]
+struct Options {
+    host: String,
+    dirs: Vec<String>,
+}
+
+fn get_path(event: &DebouncedEvent) -> Option<&std::path::Path> {
+    match event {
+        DebouncedEvent::Create(path)
+        | DebouncedEvent::Remove(path)
+        | DebouncedEvent::Chmod(path)
+        | DebouncedEvent::Write(path) => Some(path),
+        _ => None, //
+    }
+} //
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let options = Options::from_args();
+    println!("{:#?}", options);
+
+    let mut dirs_to_sync: Vec<std::path::PathBuf> = vec![];
 
     let (_, col) = pbar::window_size();
     println!("col = {}", col);
 
-    let mut x = false;
-    loop {
-        pbar::print_pbar(&entries, if x { "--" } else { "  " });
+    let (tx, rx) = channel();
+    let mut watcher = watcher(tx, time::Duration::from_secs(1)).expect("error");
 
-        let secs = time::Duration::from_secs(1);
-        thread::sleep(secs);
-
-        x = !x;
-
-        pbar::reset_pbar(entries.len());
+    for parent in options.dirs {
+        for dir in fs::read_dir(&parent)? {
+            dirs_to_sync.push(dir?.path())
+        }
+        watcher.watch(parent, RecursiveMode::Recursive)?;
     }
+    loop {
+        match rx.recv() {
+            Ok(event) => {
+                let path = get_path(&event);
+                if path.is_none() {
+                    continue;
+                }
+
+                let edited = path.unwrap();
+
+                for dir in dirs_to_sync.iter() {
+                    let mut ib = gitignore::GitignoreBuilder::new(dir.canonicalize().unwrap());
+                    ib.add(dir.join(".gitignore"));
+
+                    let ignore = ib.build().unwrap();
+
+                    let m = ignore.matched_path_or_any_parents(&edited, false);
+                    if m.is_ignore() {
+                        println!("ignore edited file {:?}", edited);
+                        continue;
+                    }
+
+                    if edited.starts_with(dir.canonicalize().unwrap()) {
+                        println!("rsync {:?} due to {:?}", dir, event);
+                    }
+                }
+            }
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
 }
