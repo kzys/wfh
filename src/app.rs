f4a75e6dddaf457eaab9de17b9e4b8eba65de346
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2020-02-08 09:57:26 -0800

    rsync must be happening at each directory separately

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,15 @@
-use ignore::gitignore;
+use ignore::gitignore::GitignoreBuilder;
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time;
 use std::time::Duration;
 
 pub struct App {
-    pub host: String,
-    pub dirs: Vec<String>,
+    host: String,
+    dirs: Vec<PathBuf>,
 }
 
 fn find_path(event: &DebouncedEvent) -> Option<&Path> {
@@ -23,26 +23,42 @@ fn find_path(event: &DebouncedEvent) -> Option<&Path> {
 }
 
 impl App {
-    fn find_dir_to_sync(&self, event: &DebouncedEvent) -> Option<PathBuf> {
-        find_path(&event).and_then(|edited|{
-            for dir in self.dirs.iter() {
-                let dir = std::path::PathBuf::from(dir);
+    pub fn new(host: String, dirs: Vec<String>) -> Result<App, Box<dyn std::error::Error>> {
+        let mut dirs_to_sync: Vec<std::path::PathBuf> = vec![];
 
-                let mut ib = gitignore::GitignoreBuilder::new(dir.clone());
-                ib.add(dir.join(".gitignore"));
+        for parent in dirs {
+            for dir in fs::read_dir(&parent)? {
+                dirs_to_sync.push(dir?.path())
+            }
+        }
+
+        Ok(App {
+            host,
+            dirs: dirs_to_sync,
+        })
+    }
+    fn find_dir_to_sync(&self, event: &DebouncedEvent) -> Option<PathBuf> {
+        find_path(&event).and_then(|edited| {
+            let dir_to_sync = self
+                .dirs
+                .iter()
+                .map(PathBuf::from)
+                .find(|dir| edited.starts_with(dir.canonicalize().unwrap()));
+
+            dir_to_sync.filter(|dir| {
+                // TODO: handle multiple .gitignore files
+                let ignore_path = dir.join(".gitignore");
+                if !ignore_path.exists() {
+                    return true;
+                }
+
+                let mut ib = GitignoreBuilder::new(dir.clone());
+                ib.add(ignore_path);
 
                 let ignore = ib.build().unwrap();
-
                 let m = ignore.matched_path_or_any_parents(&edited, false);
-                if m.is_ignore() {
-                    continue;
-                }
-
-                if edited.starts_with(dir.canonicalize().unwrap()) {
-                    return Some(dir);
-                }
-            }
-            None
+                !m.is_ignore()
+            })
         })
     }
 
@@ -64,7 +80,7 @@ impl App {
             match rx.recv_timeout(Duration::from_millis(1000)) {
                 Ok(event) => {
                     self.find_dir_to_sync(&event).map(|x| dirs_set.insert(x));
-                },
+                }
                 Err(e) => {
                     if e == std::sync::mpsc::RecvTimeoutError::Timeout {
                         if !dirs_set.is_empty() {
@@ -81,6 +97,6 @@ impl App {
     }
 
     fn sync_dirs(&self, dirs: &HashSet<PathBuf>) {
-        println!("rsync: {:?}", dirs);
+        println!("rsync: {:?} {:?}", self.host, dirs);
     }
 }
